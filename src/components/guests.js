ba47207803428ed95bac5306a5fef39d4b003a2a
Josef Jadrny <josef.jadrny@pipedrive.com>
2023-12-26 13:48:15 +0100

    timeline

--- src/components/guests.js
+++ src/components/guests.js
@@ -11,6 +11,99 @@ export default function Guests () {
               <div className="col-md-8 col-md-offset-2 text-center heading-section">
                 <h2>Program</h2>
               </div>
+              <ul class="timeline">
+  <li>
+    <div class="direction-r">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Příjezd hostů</span>
+        <span class="time-wrapper"><span class="time">11:30</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-l">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Obřad</span>
+        <span class="time-wrapper"><span class="time">13:00</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-r">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Skupinové focení</span>
+        <span class="time-wrapper"><span class="time">13:30</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-l">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Polévka + Raut</span>
+        <span class="time-wrapper"><span class="time">14:30</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-r">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Hry</span>
+        <span class="time-wrapper"><span class="time">16:00</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-l">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Krájení dortu</span>
+        <span class="time-wrapper"><span class="time">17:45</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-r">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Focení novomanželů</span>
+        <span class="time-wrapper"><span class="time">18:00</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-l">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Večerní raut</span>
+        <span class="time-wrapper"><span class="time">19:00</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-r">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">První tanec</span>
+        <span class="time-wrapper"><span class="time">19:30</span></span>
+      </div>
+    </div>
+  </li>
+  <li>
+    <div class="direction-l">
+      <div class="flag-wrapper">
+        <span class="hexa"></span>
+        <span class="flag">Party</span>
+        <span class="time-wrapper"><span class="time">22:00</span></span>
+      </div>
+    </div>
+  </li>
+</ul>
+
             </div>
             <div className="row animate-box">
 
